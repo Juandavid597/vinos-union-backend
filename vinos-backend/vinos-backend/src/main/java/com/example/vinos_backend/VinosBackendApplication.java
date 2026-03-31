3645ef854846a595341d9444286e028dc8d1d5cf
Jdperez597 <chuganeythor777@gmail.com>
2026-03-30 20:04:16 -0500

    Spring inicial
    inicializador de spring boot base

---
+++ vinos-backend/vinos-backend/src/main/java/com/example/vinos_backend/VinosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.vinos_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinosBackendApplication.class, args);
+	}
+
+}
